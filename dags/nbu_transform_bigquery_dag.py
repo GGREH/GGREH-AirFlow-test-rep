657f035dc32457191de92c050927edc45d180c8f
Roman <roma.class08@gmail.com>
2026-09-08 11:24:17 +0300

    SETL4

--- dags/nbu_transform_bigquery_dag.py
+++ dags/nbu_transform_bigquery_dag.py
@@ -1,27 +1,40 @@
+from datetime import datetime
 from airflow.decorators import dag
 from airflow.providers.google.cloud.operators.dataform import (
     DataformCreateCompilationResultOperator,
     DataformCreateWorkflowInvocationOperator,
 )
-from datetime import datetime
 
 PROJECT_ID = "project-4deacada-3830-4d03-80c"
 REGION = "us-central1"
 REPOSITORY_ID = "post_stage"
+WORKSPACE_ID = "nbu_kurs"
 
-@dag(dag_id="dataform_gcp_pipeline", start_date=datetime(2026, 9, 1), schedule=None)
+WORKSPACE_PATH = f"projects/{PROJECT_ID}/locations/{REGION}/repositories/{REPOSITORY_ID}/workspaces/{WORKSPACE_ID}"
+
+
+@dag(
+    dag_id="dataform_gcp_pipeline",
+    start_date=datetime(2026, 9, 1),
+    schedule=None,
+    catchup=False,
+    tags=["nbu", "dataform", "transform", "gcp", "worker", "child"],
+    description="Исполняющий DAG: компилирует и запускает трансформацию Dataform напрямую из GCP Workspace nbu_kurs",
+)
 def dataform_pipeline():
 
+    # 1. Компиляция Dataform напрямую из Workspace в GCP
     compile_dataform = DataformCreateCompilationResultOperator(
         task_id="compile_dataform",
         project_id=PROJECT_ID,
         region=REGION,
         repository_id=REPOSITORY_ID,
         compilation_result={
-            "git_commitish": "main"
+            "workspace": WORKSPACE_PATH,
         },
     )
 
+    # 2. Запуск выполнения моделей Dataform по тегу
     invoke_dataform = DataformCreateWorkflowInvocationOperator(
         task_id="invoke_dataform",
         project_id=PROJECT_ID,
@@ -36,5 +49,6 @@ def dataform_pipeline():
     )
 
     compile_dataform >> invoke_dataform
+
 
 dataform_pipeline()
